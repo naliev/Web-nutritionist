17a9763e5cca123f978d0c3df283aae59e9a1cbe
naliev <sms290@yandex.ru>
2022-10-17 20:28:04 +0300

    Add boundary to choose

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -6,13 +6,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-/*  public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-
- */
-    public static <T extends Comparable<? super T>> boolean isBetweenHalfOpen(T local, T start, T end) {
-        return local.compareTo(start) >= 0 && local.compareTo(end) <= 0;
+    public static <T extends Comparable<? super T>> boolean isBetween(T local, T start, T end, boolean includeRightBoundary) {
+        return local.compareTo(start) >= 0 && (includeRightBoundary ? local.compareTo(end) <= 0 : local.compareTo(end) < 0);
     }
 
     public static String toString(LocalDateTime ldt) {
