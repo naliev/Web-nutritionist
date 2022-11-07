66da00b1060e42d22249a00c23e3fe926cf83703
naliev <sms290@yandex.ru>
2022-11-07 21:29:52 +0300

    Fix results duplicating in tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
@@ -39,6 +40,11 @@ public abstract class AbstractServiceTest {
             log.info(result + " ms\n");
         }
     };
+
+    @BeforeClass
+    public static void clearResults() {
+        results.setLength(0);
+    }
 
     @AfterClass
     public static void printResult() {
